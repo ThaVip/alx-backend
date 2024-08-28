6d37fb678d30e048c79c9d2f37d1507bacf56744
ZhaVip <sheriffmusazarami@gmail.com>
2024-08-28 02:47:53 +0000

    FLASK

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python3
+"""
+Flask app for serving a simple webpage.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Handle the root route by rendering the index page.
+    :return: HTML content for the index page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
