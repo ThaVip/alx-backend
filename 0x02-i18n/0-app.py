ed76a0bdc4c5d7918216122ea7553c65cd72db2a
ZhaVip <sheriffmusazarami@gmail.com>
2024-08-28 03:25:47 +0000

    flask babel

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
-"""
-Flask app for serving a simple webpage.
-"""
+'''Task 0: Basic Flask app
+'''
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index() -> str:
-    """
-    Handle the root route by rendering the index page.
-    :return: HTML content for the index page.
-    """
-    return render_template('index.html')
+def index():
+    '''default route'''
+    return render_template("0-index.html",)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    app.run(debug=True)
